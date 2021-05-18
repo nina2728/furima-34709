54d4f6898904d40e59bd51709057cf4f0ce5a05f
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-19 07:44:36 +0900

    deviseストロングパラメーター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters
+  
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lastname_kanji, :firstname_kanji, :lastname_kana, :firstname_kana, :date_of_birth])
+  end
 end
