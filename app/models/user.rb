98ca0eeb9e464a4ad0b5878deb54c14afc972302
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-21 20:05:27 +0900

    message削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   KANJI_REGEX    = /\A[ぁ-んァ-ン一-龥]/.freeze
   KANA_REGEX     = /\A[ァ-ヶー－]+\z/.freeze
 
-  validates :password,        format: { with: PASSWORD_REGEX, message: "半角英数字混合かつ６文字以上入力"}
+  validates :password,        format: { with: PASSWORD_REGEX }
   validates :nickname,        presence: true, length: { maximum: 40}
-  validates :lastname_kanji,  presence: true, format: { with: KANJI_REGEX, message: "全角かな、全角カナ、漢字が使用できます" }
-  validates :firstname_kanji, presence: true, format: { with: KANJI_REGEX, message: "全角かな、全角カナ、漢字が使用できます" }
-  validates :lastname_kana,   presence: true, format: { with: KANA_REGEX, message: "全角カナのみ使用できます" }
-  validates :firstname_kana,  presence: true, format: { with: KANA_REGEX, message: "全角カナのみ使用できます" }
+  validates :lastname_kanji,  presence: true, format: { with: KANJI_REGEX }
+  validates :firstname_kanji, presence: true, format: { with: KANJI_REGEX }
+  validates :lastname_kana,   presence: true, format: { with: KANA_REGEX }
+  validates :firstname_kana,  presence: true, format: { with: KANA_REGEX }
   validates :date_of_birth,   presence: true
 end
