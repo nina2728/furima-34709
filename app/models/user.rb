a47a6ce18e4994c89b69e84fa02eae409cb7768f
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-22 18:36:23 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  KANJI_REGEX    = /\A[ぁ-んァ-ン一-龥]/.freeze
+  KANJI_REGEX    = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
   KANA_REGEX     = /\A[ァ-ヶー－]+\z/.freeze
 
   validates :password,        format: { with: PASSWORD_REGEX }
