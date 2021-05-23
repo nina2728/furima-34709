fcc7153dafcb3d848ddd974afbf14068e92dccd3
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-23 16:22:02 +0900

    ストロングパラメーター記述ミス修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_id, :prefecture_id, :shipping_id, :price, :image)merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_id, :prefecture_id, :shipping_id, :price, :image).merge(user_id: current_user.id)
   end
 end
