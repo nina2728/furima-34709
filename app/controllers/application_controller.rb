d4d2d3ce366d8b1105873613f976aed70edef669
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-23 12:54:14 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_authx
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   private
+  def basic_authx
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTHX_USER"] && password == ENV["BASIC_AUTHX_PASSWORD"]
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lastname_kanji, :firstname_kanji, :lastname_kana, :firstname_kana, :date_of_birth])
   end
