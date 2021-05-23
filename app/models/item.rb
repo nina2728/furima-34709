79960bd69d97933783108a66919a3f67d84ed2a7
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-23 19:39:27 +0900

    ActiveHashアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one    :buy
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :shipping
+  belongs_to :status
 end
