8a829c18b5561dc52b6afb2ff7f888d569b5e0dd
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-23 19:52:08 +0900

    ActiveHashバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping
   belongs_to :status
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    with_options numericality: { order_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_id
+      validates :prefecture_id
+      validates :shipping_id
+    end
+  end 
 end
