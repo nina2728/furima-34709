3b708b427022f644818586d63f40df8d78b0f024
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-25 20:58:11 +0900

    factorybot記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :item do
-    
+    image = fixture_file_upload("/files/test_image.png") 
+    name        {Faker::Pokemon.name}
+    description {Faker::Lorem.sentences}
+    price       {Faker::Number.within(range: 300..9999999)}
+    ah_id       {Faker::Number.within(range: 2..4)}
   end
 end
