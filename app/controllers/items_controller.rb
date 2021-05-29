7cc22985ffd6fdfce3ccd8536e647ed8f42a35b2
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-30 08:46:31 +0900

    コントローラーの追加、修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
     item = Item.find(params[:id])
-    item.update(item.params)
+    item.update(item_params)
   end
 
   private
