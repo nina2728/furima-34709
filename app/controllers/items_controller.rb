c0aecd178fc88c93812a5d472d517c5a8a3326c8
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-30 11:59:59 +0900

    DRYの規則に従ったコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_info, only: [:show, :edit, :update]
+  before_action :item_authenticate, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,20 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if @item.user == current_user
-      render :edit
-    else
-      redirect_to root_path
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
@@ -45,5 +39,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_id, :prefecture_id, :shipping_id,
                                  :price, :image).merge(user_id: current_user.id)
+  end
+
+  def item_info
+    @item = Item.find(params[:id])
+  end
+
+  def item_authenticate
+    unless @item.user == current_user
+      redirect_to root_path
+    end
   end
 end
