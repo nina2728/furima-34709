9521cddc0b6c0751ced07d2060f33050531c4847
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-06-07 20:44:28 +0900

    createアクション定義

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,8 +1,20 @@
 class BuysController < ApplicationController
   def index
-    @buyreceiver = BuyReceiver.new
+    @buy_receiver = BuyReceiver.new
   end
 
   def create
+    @buy_receiver = BuyReceiver.create(buy_receiver_params)
+    if @buy_receiver.valid?
+      @buy_receiver.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def buy_receiver_params
+    params.require(:buy_receiver).permit(:item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :buy_id).merge(:user_id current_user.id)
   end
 end
