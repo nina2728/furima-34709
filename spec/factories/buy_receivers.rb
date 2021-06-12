353cb049ebc66baabb9fc779b94fb680ea0b4c30
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-06-12 12:59:06 +0900

    テストコード修正

--- spec/factories/buy_receivers.rb
+++ spec/factories/buy_receivers.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     municipality  { Faker::Address.state }
     address       { Faker::Address.street_address }
     building_name { Faker::Address.street_name }
-    phone_number  { Faker::Number.within(range: 1..11) }
+    phone_number  { Faker::PhoneNumber.subscriber_number(length: 11) }
     token         { Faker::Crypto.md5 }
   end
 end
